7906085d83d84659b13440990b6032817d4de66d
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2026-03-24 17:30:13 +0500

    Add sentinel log path to jails configuration for enhanced monitoring

--- install.sh
+++ install.sh
@@ -435,6 +435,7 @@ enabled  = true
 port     = http,https
 filter   = sentinel-scanner
 logpath  = /var/log/nginx/*_access.log
+           /var/log/nginx/*_sentinel.log
            /var/log/nginx/access.log
 maxretry = 3
 findtime = 600
@@ -447,6 +448,7 @@ enabled  = true
 port     = http,https
 filter   = sentinel-exploit
 logpath  = /var/log/nginx/*_access.log
+           /var/log/nginx/*_sentinel.log
            /var/log/nginx/access.log
 maxretry = 1
 findtime = 600
@@ -459,6 +461,7 @@ enabled  = true
 port     = http,https
 filter   = sentinel-botnet
 logpath  = /var/log/nginx/*_access.log
+           /var/log/nginx/*_sentinel.log
            /var/log/nginx/access.log
 maxretry = 1
 findtime = 600
@@ -471,6 +474,7 @@ enabled  = true
 port     = http,https
 filter   = sentinel-ratelimit
 logpath  = /var/log/nginx/*_access.log
+           /var/log/nginx/*_sentinel.log
            /var/log/nginx/access.log
 maxretry = 100
 findtime = 60
@@ -483,6 +487,7 @@ enabled  = true
 port     = http,https
 filter   = sentinel-bruteforce
 logpath  = /var/log/nginx/*_access.log
+           /var/log/nginx/*_sentinel.log
            /var/log/nginx/access.log
 maxretry = 15
 findtime = 300
